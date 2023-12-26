48961cadbfc37bbcb607dfcef16ac131ed76a72e
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-26 09:42:58 +0100

    test

---
+++ gsplat/src/main.ts
@@ -0,0 +1,40 @@
+import * as SPLAT from "gsplat";
+
+const canvas = document.getElementById("canvas");
+const progressDialog = document.getElementById("progress-dialog");
+const progressIndicator = document.getElementById("progress-indicator");
+
+const renderer = new SPLAT.WebGLRenderer(canvas);
+const scene = new SPLAT.Scene();
+const camera = new SPLAT.Camera();
+const controls = new SPLAT.OrbitControls(camera, canvas);
+
+async function main() {
+  const url =
+    "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/bonsai-7k-mini.splat";
+
+  await SPLAT.Loader.LoadAsync(
+    url,
+    scene,
+    (progress) => (progressIndicator.value = progress * 100)
+  );
+  progressDialog.close();
+
+  const handleResize = () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  };
+
+  const frame = () => {
+    controls.update();
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(frame);
+  };
+
+  handleResize();
+  window.addEventListener("resize", handleResize);
+
+  requestAnimationFrame(frame);
+}
+
+main();
